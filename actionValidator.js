0bbfca03bd02e7fa106f6416bf3a52333edabfd9
ankit-sharechat <ankit.kumar@sharechat.co>
2022-10-03 14:22:08 +0530

    refactor actionValidator.js

---
+++ actionValidator.js
@@ -0,0 +1,36 @@
+const {WebCardNavigation} = require("./constants");
+
+function validateViewActionRef(actionSource, eventSource, dataObject, actionRef, referrer) {
+    if (actionRef !== undefined) {
+        //Validate Css Ref is a list
+        if (actionSource.view[actionRef] === undefined) {
+            error("ViewActionNotFound: `" + actionRef + "` in actionSource.view{}. Info: { " + referrer + " }")
+        } else {
+            const actionObject = actionSource.view[actionRef]
+            validateEventObject(eventSource, dataObject, actionObject.eventRef, referrer+"."+actionRef)
+        }
+    }
+}
+
+function validateClickActionRef(actionSource, eventSource, dataObject, actionRef, referrer) {
+    if (actionRef !== undefined) {
+        const clickActionObject = actionSource.click[actionRef]
+        if (clickActionObject === undefined) {
+            error("ClickActionNotFound: `" + actionRef + "` in actionSource.click. Info: { " + referrer + " }")
+        } else {
+
+            if (clickActionObject.type !== WebCardNavigation) {
+                error("ActionTypeError: " + clickActionObject.type + ". Required `" + WebCardNavigation + "` | Info: " + referrer)
+            }
+
+            if (clickActionObject.webCard === undefined) {
+                error("MissingObject: webCard | Info: " + referrer)
+            } else {
+                //validate placeholders
+                validatePlaceHolder(clickActionObject.webCard, dataObject, referrer)
+            }
+
+            validateEventRef(eventSource, clickActionObject.eventRef, dataObject, referrer+" | ActionNode: actionSource.click."+actionRef+"."+clickActionObject.eventRef)
+        }
+    }
+}
